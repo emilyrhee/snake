8e275c624429601c725b5ed678410679dface2a7
emilyrhee <emilyrhee99@gmail.com>
2024-04-16 22:06:32 -0400

    added drawBorders()

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <curses.h>
-#include <stdio.h>
+void drawBorders() { // Emily
+    for (int i = 0; i < COLS; i++) { // Draw top & bottom lines
+        move(0, i);
+        addstr("-");
+        move(LINES - 1, i);
+        addstr("-");
+    }
+
+    for (int i = 0; i < LINES; i++) { // Draw side lines
+        move(i, 0);
+        addstr("|");
+        move(i, COLS - 1);
+        addstr("|");
+    }
+}
 
 int main() {
     return 0;
