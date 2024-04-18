61f6591c2845586b49a17ca2e36758cc668a1504
mitchp819 <mitchellpalczewski@gmail.com>
2024-04-18 00:31:38 +0000

    struct snakeData: int alive, int size, int x [], int y [] for each body piece.
    void initSnake: at center
    
    TODO: pick char for snake, how to do in game frames work, make snake move

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <curses.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define MAXSNAKELENGTH 20
+#define INITSIZE 3
+
+//defines snake (Mitch)
+struct snakeData{
+    int alive;
+    int size; 
+    int x [MAXSNAKELENGTH];
+    int y [MAXSNAKELENGTH];
+};
+
+//initializes snake (Mitch)
+void initSnake(struct snakeData* snakeAlias){
+    snakeAlias -> size = INITSIZE; 
+    snakeAlias -> alive = 1;
+    for(int i = 0; i < INITSIZE; i++){
+        snakeAlias -> x[i] = COLS / 2 - i;
+        snakeAlias -> y[i] = LINES / 2;
+        move(LINES / 2, COLS / 2 - i);
+        addstr("#");
+    }
+}
 
 // boilerplate for curses (Emily)
 void initCurses() {
@@ -39,13 +63,19 @@ void handleInput() {
     }
 }
 
-int main() {
-    initCurses();
 
+int main() {
+    struct snakeData snake; 
+
+    initCurses();
     drawBorders();
+    initSnake(&snake);
     
-    while(1) {
-        handleInput();
+    while(snake.alive){
+        //TODO snake movement goes here (I Think)
+        while(1) {
+            handleInput();
+        }
     }
 
     return 0;
